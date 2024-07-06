9259dd1df29948577494cbef3c4f123795c21ddd
IKBAL <sikbalahmad007@gmail.com>
2024-07-06 23:51:04 +0530

    abc

--- route/order.js
+++ route/order.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const create_order = require("../controller/order/create");
-const get = require("../controller/order/get");
-const get_by_id = require("../controller/order/get_id");
+const get = require("../controller/order/get_by_restaurant_id");
+const get_by_id = require("../controller/order/get_by_id");
 const update = require("../controller/order/update");
 const delete_by_id = require("../controller/order/delete");
-const customer_order=require("../controller/order/customer_order");
 const customer_list=require("../controller/order/list");
 
 
 router.post("/create", create_order);
 router.get("/get/:restaurant_id", get);
-router.get("/customer_order",customer_order);
 router.get("/get_id/:id",get_by_id);
 router.get("/customer_list",customer_list);
 router.put("/update/:id", update);
