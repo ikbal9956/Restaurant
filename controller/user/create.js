a2c93600fbbcff2435fbf3bd295f6c7bd0345408
IKBAL <sikbalahmad007@gmail.com>
2024-07-19 23:33:50 +0530

    abc

--- controller/user/create.js
+++ controller/user/create.js
@@ -1,9 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
 const userModel = require("../../model/user/user.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const createToken = require("../../utlis/create_token.js");
+
 
 const create_order = (req, res) => {
-  const { name, email, password ,is_admin =false } = req.body;
+  const { name, email, password, is_admin = false } = req.body;
 
   bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) {
@@ -14,7 +17,7 @@ const create_order = (req, res) => {
       id: uuidv4(),
       name,
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       is_admin,
       is_active: true,
       created_at: new Date(),
@@ -23,9 +26,17 @@ const create_order = (req, res) => {
 
     userModel.create_user(userData, (err, data) => {
       if (err) return res.status(500).json(err);
-      return res
-        .status(201)
-        .json({ message: "user created successfully", data });
+
+      const tokenData = createToken(userData.id, userData.is_admin);
+      return res.status(201).json({
+        message: "user created successfully",
+        token: tokenData.token,
+        expires_in: tokenData.expiresIn,
+        is_admin: userData.is_admin,
+        data: {
+          user: userData,
+        },
+      });
     });
   });
 };
