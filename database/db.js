6167e436f8355e0954f92e4e53aa85d4b457f25e
IKBAL <sikbalahmad007@gmail.com>
2024-09-24 01:40:32 +0530

    abc

--- database/db.js
+++ database/db.js
@@ -1,25 +1,3 @@
-// const mysql = require("mysql");
-
-// const db = mysql.createConnection({
-  
-//   host:"online123-5483.7s5.aws-ap-south-1.cockroachlabs.cloud",
-//   port:"26257",
-//   database: "defaultdb",
-//   user:"ikbal",
-//   password:"Online123"
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Database connection failed: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to database.");
-// });
-
-// module.exports = db;
-
-
 const { Client } = require('pg');
 
 const client = new Client({
